aaac956f59a9ae9b72df2746d689be1868a776c5
Linus <linus-tiemann@live.de>
2021-06-13 15:44:50 +0200

    added former files to the project

---
+++ CSharpSourceCode/CampaignMode/WorldMapAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using SandBox;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
+using TaleWorlds.SaveSystem;
+
+namespace TOW_Core.CampaignMode
+{
+    public class WorldMapAttribute
+    {
+        [SaveableField(1)]
+        public Hero Leader;
+        [SaveableField(2)]
+        public string id;
+
+        public string culture;
+
+        public bool MagicUsers;
+        public float WindsOfMagic;
+        
+        
+
+        public WorldMapAttribute(string id)
+        {
+            this.id = id;
+        }
+
+        public WorldMapAttribute()
+        {
+
+        }
+    }
+}
