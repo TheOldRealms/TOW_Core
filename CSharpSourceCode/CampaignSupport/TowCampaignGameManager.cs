f4ab601a3fa4fa39ce8fd6f344159278698d063a
tristanka <tristanarm@gmail.com>
2021-07-05 23:24:38 -0500

    Fix spacing

--- CSharpSourceCode/CampaignSupport/TowCampaignGameManager.cs
+++ CSharpSourceCode/CampaignSupport/TowCampaignGameManager.cs
@@ -15,39 +15,39 @@ namespace TOW_Core.CampaignSupport
 {
     class TowCampaignGameManager : SandBoxGameManager
     {
-		public override void OnLoadFinished()
-		{
-			VideoPlaybackState videoPlaybackState = Game.Current.GameStateManager.CreateState<VideoPlaybackState>();
-			string towFullPath = ModuleHelper.GetModuleFullPath("TOW_Core");
-			string sandboxFullPath = ModuleHelper.GetModuleFullPath("SandBox");
-			string towIntroPath = towFullPath + "Videos/tow_intro.ivf";
-			string sandboxIntroPath = sandboxFullPath + "Videos/campaign_intro.ivf";
-			if (File.Exists(towIntroPath))
+        public override void OnLoadFinished()
+        {
+            VideoPlaybackState videoPlaybackState = Game.Current.GameStateManager.CreateState<VideoPlaybackState>();
+            string towFullPath = ModuleHelper.GetModuleFullPath("TOW_Core");
+            string sandboxFullPath = ModuleHelper.GetModuleFullPath("SandBox");
+            string towIntroPath = towFullPath + "Videos/tow_intro.ivf";
+            string sandboxIntroPath = sandboxFullPath + "Videos/campaign_intro.ivf";
+            if (File.Exists(towIntroPath))
             {
-				string audioPath = towFullPath + "Videos/tow_intro.ogg";
-				string subtitleFileBasePath = towFullPath + "Videos/tow_intro";
-				videoPlaybackState.SetStartingParameters(towIntroPath, audioPath, subtitleFileBasePath, 30f, true);
-				videoPlaybackState.SetOnVideoFinisedDelegate(new Action(this.LaunchStoryModeCharacterCreation));
-			}
-			else
+                string audioPath = towFullPath + "Videos/tow_intro.ogg";
+                string subtitleFileBasePath = towFullPath + "Videos/tow_intro";
+                videoPlaybackState.SetStartingParameters(towIntroPath, audioPath, subtitleFileBasePath, 30f, true);
+                videoPlaybackState.SetOnVideoFinisedDelegate(new Action(this.LaunchStoryModeCharacterCreation));
+            }
+            else
             {
-				string audioPath = towFullPath + "Videos/campaign_intro.ogg";
-				string subtitleFileBasePath = towFullPath + "Videos/campaign_intro";
-				videoPlaybackState.SetStartingParameters(sandboxIntroPath, audioPath, subtitleFileBasePath, 60f, true);
-				videoPlaybackState.SetOnVideoFinisedDelegate(new Action(this.LaunchStoryModeCharacterCreation));
-			}
-			
-			Game.Current.GameStateManager.CleanAndPushState(videoPlaybackState, 0);
-			base.IsLoaded = true;
-		}
+                string audioPath = towFullPath + "Videos/campaign_intro.ogg";
+                string subtitleFileBasePath = towFullPath + "Videos/campaign_intro";
+                videoPlaybackState.SetStartingParameters(sandboxIntroPath, audioPath, subtitleFileBasePath, 60f, true);
+                videoPlaybackState.SetOnVideoFinisedDelegate(new Action(this.LaunchStoryModeCharacterCreation));
+            }
+            
+            Game.Current.GameStateManager.CleanAndPushState(videoPlaybackState, 0);
+            base.IsLoaded = true;
+        }
 
-		private void LaunchStoryModeCharacterCreation()
-		{
-			CharacterCreationState gameState = Game.Current.GameStateManager.CreateState<CharacterCreationState>(new object[]
-			{
-				new StoryModeCharacterCreationContent()
-			});
-			Game.Current.GameStateManager.CleanAndPushState(gameState, 0);
-		}
-	}
+        private void LaunchStoryModeCharacterCreation()
+        {
+            CharacterCreationState gameState = Game.Current.GameStateManager.CreateState<CharacterCreationState>(new object[]
+            {
+                new StoryModeCharacterCreationContent()
+            });
+            Game.Current.GameStateManager.CleanAndPushState(gameState, 0);
+        }
+    }
 }
