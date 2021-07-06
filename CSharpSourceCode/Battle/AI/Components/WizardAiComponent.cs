deeec1ad5b9c258d8984d7a2944e3dc0a7b3d785
Mart Kartasev <mart.kartasev@gmail.com>
2021-07-06 21:42:50 +0300

    Utility value modification for ranged only AI

---
+++ CSharpSourceCode/Battle/AI/Components/WizardAiComponent.cs
@@ -0,0 +1,20 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace TOW_Core.Battle.AI.Components
+{
+    public class WizardAiComponent : HumanAIComponent
+    {
+        public WizardAiComponent(Agent agent) : base(agent)
+        {
+            SetBehaviorParams(AISimpleBehaviorKind.Melee, 0, 5, 0, 20, 0);
+            SetBehaviorParams(AISimpleBehaviorKind.ChargeHorseback, 0, 7, 0, 30, 0);
+        }
+
+        public override void OnTickAsAI(float dt)
+        {
+            base.OnTickAsAI(dt);
+            SetBehaviorParams(AISimpleBehaviorKind.Melee, 0, 5, 0, 20, 0);
+            SetBehaviorParams(AISimpleBehaviorKind.ChargeHorseback, 0, 7, 0, 30, 0);
+        }
+    }
+}
---
+++ CSharpSourceCode/Battle/AI/CustomAIMissionLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using TaleWorlds.MountAndBlade;
+using TOW_Core.Battle.AI.Components;
+using TOW_Core.Battle.Extensions;
+using TOW_Core.Utilities;
+
+namespace TOW_Core.Battle.AI
+{
+    public class CustomAiMissionLogic : MissionLogic
+    {
+        public override void OnAgentCreated(Agent agent)
+        {
+            base.OnAgentCreated(agent);
+            if (agent != Agent.Main && agent.IsAbilityUser())
+            {
+                var toRemove = agent.Components.OfType<HumanAIComponent>().ToList();
+                foreach (var item in toRemove)
+                    agent.RemoveComponent(item);
+                TOWCommon.Say("Added Wizard component");
+                agent.AddComponent(new WizardAiComponent(agent));
+            }
+        }
+    }
+}
+
