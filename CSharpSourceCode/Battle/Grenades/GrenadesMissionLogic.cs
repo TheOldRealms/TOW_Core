1935be77647872da265f17556650d3cbdc876b00
Badulundabad <greenday407@mail.ru>
2021-07-28 21:57:35 +0300

    Fixed the grenade logics, reworked by TriggeredEffect

--- CSharpSourceCode/Battle/Grenades/GrenadesMissionLogic.cs
+++ CSharpSourceCode/Battle/Grenades/GrenadesMissionLogic.cs
@@ -1,26 +1,23 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.ObjectSystem;
+using TOW_Core.Battle.TriggeredEffect;
+using TOW_Core.Battle.TriggeredEffect.Scripts;
 
 namespace TOW_Core.Battle.Grenades
 {
     public class GrenadesMissionLogic : MissionLogic
     {
-        private uint currentMissileId = 0;
-
-        public GrenadesMissionLogic()
-        {
-
-        }
-
         public override void OnMissionTick(float dt)
         {
             base.OnMissionTick(dt);
-            if (Input.IsKeyPressed(InputKey.L))
+            if (!(Game.Current.GameType is Campaign) && Input.IsKeyPressed(InputKey.L))
                 DebugOnlyEquipGrenades();
         }
         public override void OnAgentShootMissile(Agent shooterAgent, EquipmentIndex weaponIndex, Vec3 position, Vec3 velocity, Mat3 orientation, bool hasRigidBody, int forcedMissileIndex)
@@ -28,10 +25,10 @@ namespace TOW_Core.Battle.Grenades
             base.OnAgentShootMissile(shooterAgent, weaponIndex, position, velocity, orientation, hasRigidBody, forcedMissileIndex);
             if (shooterAgent.WieldedWeapon.Item.Name.Contains("Grenade"))
             {
-                shooterAgent.WieldedWeapon.Item.Id = new MBGUID(currentMissileId);
-                Mission.Missile grenade = Mission.Current.Missiles.ToList().FirstOrDefault(m => m.Weapon.Item.Id.InternalValue == currentMissileId && m.Weapon.Item.Name.Contains("Grenade"));
-                currentMissileId++;
-                ActivateIfGrenade(grenade);
+                Mission.Missile grenade = Mission.Missiles.FirstOrDefault(m => m.ShooterAgent == shooterAgent &&
+                                                                               m.Weapon.Item.Name.Contains("Grenade") &&
+                                                                               !m.Entity.HasScriptOfType<GrenadeScript>());
+                if (grenade != null) Activate(grenade);
             }
         }
         public override void OnAgentHit(Agent affectedAgent, Agent affectorAgent, int damage, in MissionWeapon affectorWeapon)
@@ -39,8 +36,7 @@ namespace TOW_Core.Battle.Grenades
             base.OnAgentHit(affectedAgent, affectorAgent, damage, affectorWeapon);
             if (affectorWeapon.Item != null && affectorWeapon.Item.Name.Contains("Grenade"))
             {
-                MissionWeapon weapon = affectorWeapon;
-                Mission.Missile grenade = Mission.Current.Missiles.ToList().FirstOrDefault(m => m.Weapon.Item.Id == weapon.Item.Id && m.Weapon.Item.Name.Contains("Grenade"));
+                Mission.Missile grenade = Mission.Current.Missiles.ToList().FirstOrDefault(m => m.Weapon.Item.Name.Contains("Grenade") && IsNearbyAgent(m, affectedAgent));
                 if (grenade != null)
                 {
                     GameEntity bouncedGrenade = grenade.Entity;
@@ -50,20 +46,27 @@ namespace TOW_Core.Battle.Grenades
                 }
             }
         }
-        private void ActivateIfGrenade(Mission.Missile grenade)
+        private void Activate(Mission.Missile grenade)
         {
             GameEntity grenadeEntity = grenade.Entity;
             grenadeEntity.CreateAndAddScriptComponent("GrenadeScript");
             GrenadeScript grenadeScript = grenadeEntity.GetFirstScriptOfType<GrenadeScript>();
             grenadeScript.SetShooterAgent(grenade.ShooterAgent);
+            grenadeScript.SetTriggeredEffect(TriggeredEffectManager.CreateNew("explosion_01"));
             grenadeEntity.CallScriptCallbacks();
         }
-
+        private bool IsNearbyAgent(Mission.Missile missile, Agent agent)
+        {
+            return Math.Abs(missile.GetPosition().X - agent.Position.X) < 0.8f && Math.Abs(missile.GetPosition().Y - agent.Position.Y) < 0.8f;
+        }
         private void DebugOnlyEquipGrenades()
         {
-            ItemObject itemObject = MBObjectManager.Instance.GetObject<ItemObject>("dwarf_hand_grenade");
-            MissionWeapon grenades = new MissionWeapon(itemObject, null, Banner.CreateRandomBanner());
-            Agent.Main.EquipWeaponToExtraSlotAndWield(ref grenades);
+            if (Agent.Main != null)
+            {
+                ItemObject itemObject = MBObjectManager.Instance.GetObject<ItemObject>("dwarf_hand_grenade");
+                MissionWeapon grenades = new MissionWeapon(itemObject, null, Banner.CreateRandomBanner());
+                Agent.Main.EquipWeaponToExtraSlotAndWield(ref grenades);
+            }
         }
     }
 }
--- CSharpSourceCode/Battle/TriggeredEffect/Scripts/GrenadeScript.cs
+++ CSharpSourceCode/Battle/TriggeredEffect/Scripts/GrenadeScript.cs
@@ -2,20 +2,17 @@
 using TaleWorlds.Engine;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
-using TOW_Core.Utilities;
 
-namespace TOW_Core.Battle.Grenades
+namespace TOW_Core.Battle.TriggeredEffect.Scripts
 {
     public class GrenadeScript : ScriptComponentBehaviour
     {
         private bool hasExploded = false;
         private bool hasLaunched = false;
         private Int32 explosionTimer = 0;
-        private const Int32 maxDamage = 110;
-        private const float radius = 5f;
         private SoundEvent _tickSound;
-        private SoundEvent _explosionSound;
         private Agent shooterAgent;
+        private TriggeredEffect explsion;
 
         protected override void OnInit()
         {
@@ -44,7 +41,7 @@ namespace TOW_Core.Battle.Grenades
             {
                 hasExploded = true;
                 _tickSound.Release();
-                ExplodeGrenade(GameEntity);
+                explsion.Trigger(GameEntity.GlobalPosition, Vec3.Zero, shooterAgent);
             }
         }
         protected override TickRequirement GetTickRequirement()
@@ -54,27 +51,15 @@ namespace TOW_Core.Battle.Grenades
         protected override void OnRemoved(int removeReason)
         {
             base.OnRemoved(removeReason);
-            if (_explosionSound != null) _explosionSound.Release();
-        }
-        private void ExplodeGrenade(GameEntity entity)
-        {
-            entity.FadeOut(2.9f, true);
-            Vec3 position = entity.GlobalPosition;
-            TOWBattleUtilities.DamageAgentsWithGrenade(position.AsVec2, radius, maxDamage, shooterAgent, false);
-            var explosion = GameEntity.CreateEmpty(Mission.Current.Scene);
-            MatrixFrame frame = MatrixFrame.Identity;
-            ParticleSystem.CreateParticleSystemAttachedToEntity("psys_burning_projectile_default_coll", explosion, ref frame);
-            var globalFrame = new MatrixFrame(Mat3.CreateMat3WithForward(in Vec3.Zero), position);
-            explosion.SetGlobalFrame(globalFrame);
-            explosion.FadeOut(3, true);
-            Int32 _explosionSoundindex = SoundEvent.GetEventIdFromString("fireball_explosion");
-            _explosionSound = SoundEvent.CreateEvent(_explosionSoundindex, Mission.Current.Scene);
-            _explosionSound.SetPosition(globalFrame.origin);
-            _explosionSound.Play();
+            _tickSound.Release();
         }
         public void SetShooterAgent(Agent shooter)
         {
             shooterAgent = shooter;
+        }
+        public void SetTriggeredEffect(TriggeredEffect effect)
+        {
+            explsion = effect;
         }
     }
 }
