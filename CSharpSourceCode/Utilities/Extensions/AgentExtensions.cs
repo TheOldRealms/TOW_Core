681c2407462eb8c9c5fe367ac430e32449dc1df0
hunharibo <hunharibo@gmail.com>
2021-08-07 19:53:13 +0200

    small change

--- CSharpSourceCode/Utilities/Extensions/AgentExtensions.cs
+++ CSharpSourceCode/Utilities/Extensions/AgentExtensions.cs
@@ -162,6 +162,7 @@ namespace TOW_Core.Utilities.Extensions
                     var blow = new Blow();
                     blow.InflictedDamage = damageAmount;
                     blow.DefenderStunPeriod = 0;
+                    blow.BlowFlag = BlowFlags.NoSound;
                     if (hasShockWave)
                     {
                         if (agent.HasMount)
@@ -171,8 +172,8 @@ namespace TOW_Core.Utilities.Extensions
                     }
                     if (damager != null)
                     {
-                        blow.OwnerId = damager.Index;
-                        blow.BlowFlag = BlowFlags.NoSound;
+                        var agentAlive = Mission.Current.FindAgentWithIndex(damager.Index);
+                        if(agentAlive != null && agentAlive.State == AgentState.Active) blow.OwnerId = damager.Index;
                     }
                     if (agent.Health > damageAmount)
                     {
