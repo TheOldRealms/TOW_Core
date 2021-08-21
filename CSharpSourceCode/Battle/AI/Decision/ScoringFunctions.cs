da13760475680198a7caa88ef809b946e1a3a825
Mart Kartasev <mart.kartasev@gmail.com>
2021-08-21 18:56:05 +0300

    Added logistic function

---
+++ CSharpSourceCode/Battle/AI/Decision/ScoringFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TOW_Core.Battle.AI.Decision
+{
+    public static class ScoringFunctions
+    {
+        public static Func<float, float> Logistic(float mid = 0.0f, float k = 1.0f, Func<float, float> input = null)
+        {
+            if (input == null)
+            {
+                return x => (float) (1 / (1 + Math.Pow(Math.E, -k * (x - mid))));
+            }
+
+            return x => (float) (1 / (1 + Math.Pow(Math.E, -k * (input.Invoke(x) - mid))));
+        }
+    }
+}
