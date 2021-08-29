3b6a28656ee9664bb7d4be5528130de65615dc5a
oleg.bulba <oleh.bulba@bini.games>
2021-08-29 14:48:22 +0300

    Made code cleaner, now wont crush game in main menu, works for mage starting character, but won't work fully on non mage characters

--- CSharpSourceCode/Abilities/ConsoleComands/AddSpellsFromConsole.cs
+++ CSharpSourceCode/Abilities/ConsoleComands/AddSpellsFromConsole.cs
@@ -7,72 +7,94 @@ using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using TOW_Core.Abilities;
 using TOW_Core.Utilities.Extensions;
 
-namespace TOW_Core.Abilities.ConsoleComands
+namespace TOW_Core.Spells.ConsoleComands
 {
     public class AddSpellsFromConsole
     {
-        private static List<string> spells;
+        private static List<string> towSpellNames = AbilityFactory.GetAllSpellNamesAsList();
 
-        static AddSpellsFromConsole()
+        private static readonly string invalidCallMessage =
+            "Baby don't break me \n" +
+            "Don't break me\n" +
+            "No - no!\n" +
+            "\n" +
+            "Load TOW save, seriously.\n";
+
+
+        [CommandLineFunctionality.CommandLineArgumentFunction("list_spells", "tow")]
+        public static string ListSpells(List<string> argumentNames) =>
+            AggregateOutput("Available spells are:", towSpellNames);
+
+        [CommandLineFunctionality.CommandLineArgumentFunction("list_player_spells", "tow")]
+        public static string ListPlayerSpells(List<string> argumentNames) =>
+            !CampaignCheats.CheckCheatUsage(ref CampaignCheats.ErrorType)
+                ? CampaignCheats.ErrorType
+                : AggregateOutput("Player got this spells:", Hero.MainHero.GetExtendedInfo().AllAbilities);
+
+        [CommandLineFunctionality.CommandLineArgumentFunction("add_spells_to_player", "tow")]
+        public static string AddSpells(List<string> arguments)
         {
-            spells = AbilityFactory.GetAllSpellNamesAsList();
-        }
+            if (!CampaignCheats.CheckCheatUsage(ref CampaignCheats.ErrorType))
+                return CampaignCheats.ErrorType;
 
-        [CommandLineFunctionality.CommandLineArgumentFunction("list_abilities", "tow")]
-        public static string ListAbilities(List<string> argumentNames) =>
-            spells.Aggregate(
-                "Available abilities are: \n",
-                (current, ability) =>
-                    $"{current}{ability} \n");
+            var matchedArguments = new List<string>();
+            var newSpells = new List<string>();
+            var knownSpells = new List<string>();
 
-        [CommandLineFunctionality.CommandLineArgumentFunction("add_ability", "tow")]
-        public static string AddAbilities(List<string> argumentNames)
-        {
-            if (Hero.MainHero == null)
-            {
-                return "Baby don't break me \n" +
-                       "Don't break me \n" +
-                       "No - no! \n" +
-                       "\n" +
-                       "Load TOW save, seriously.";
-            }
-
-            string log;
-
-            var matchedAbilities = new List<string>();
-
-            foreach (var argumentName in argumentNames)
-            foreach (var knownSpell in spells)
-                if (string.Equals(knownSpell, argumentName, StringComparison.CurrentCultureIgnoreCase))
-                    matchedAbilities.Add(knownSpell);
-
-            log = matchedAbilities.Aggregate(
-                "Matched abilities are : \n",
-                (current, ability) =>
-                    $"{current}{ability} \n");
-
-            log += "Already known abilities in request: \n";
-
-            var newAbilities = new List<string>();
-
-            foreach (var ability in matchedAbilities)
-                if (Hero.MainHero.HasAbility(ability))
-                    log += ability + "\n";
-                else
+            foreach (var argument in arguments)
+            foreach (var towSpell in towSpellNames)
+                if (string.Equals(towSpell, argument, StringComparison.CurrentCultureIgnoreCase))
                 {
-                    Hero.MainHero.AddAbility(ability);
-                    newAbilities.Add(ability);
+                    matchedArguments.Add(towSpell);
+
+                    if (Hero.MainHero.HasAbility(towSpell))
+                        knownSpells.Add(towSpell);
+                    else
+                    {
+                        Hero.MainHero.AddAbility(towSpell);
+                        newSpells.Add(towSpell);
+                    }
                 }
 
-            log += "Added abilities : \n";
+            if (newSpells.Count > 0)
+            {
+                if (!Hero.MainHero.IsSpellCaster())
+                    Hero.MainHero.AddAttribute("SpellCaster");
 
-            log = newAbilities.Aggregate(log,
-                (current, newAbility) =>
-                    current + (newAbility + " \n"));
+                if (!Hero.MainHero.IsAbilityUser())
+                    Hero.MainHero.AddAttribute("AbilityUser");
+            }
 
-            return log;
+            return FormatOutput(matchedArguments, knownSpells, newSpells);
+        }
+
+        private static string FormatOutput(List<string> matchedArguments, List<string> knownSpells,
+        List<string> newSpells) =>
+            AggregateOutput("Matched spells:", matchedArguments) +
+            AggregateOutput("Already known spells in request:", knownSpells) +
+            AggregateOutput("Added spells :", newSpells
+            );
+
+        private static string AggregateOutput(string topicHeader, List<string> matchedSpells) =>
+            matchedSpells.Aggregate(
+                $"\n{topicHeader}\n",
+                (current, spell) =>
+                    $"{current}{spell}\n"
+            );
+
+        private static List<string> FindValidSpellsInArguments(List<string> argumentNames)
+        {
+            var matchedSpells = new List<string>();
+
+            foreach (var argumentName in argumentNames)
+            foreach (var knownSpell in towSpellNames)
+                if (string.Equals(knownSpell, argumentName, StringComparison.CurrentCultureIgnoreCase))
+                    matchedSpells.Add(knownSpell);
+            return matchedSpells;
         }
     }
 }
