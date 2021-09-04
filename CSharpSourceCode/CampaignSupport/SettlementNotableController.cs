ed232acfeb0ee866625e2e5611b79e506dda0cb9
Dmitriy <greendmitriy407@gmail.com>
2021-09-05 00:16:27 +0300

    Added SettlementNotableController behavior

---
+++ CSharpSourceCode/CampaignSupport/SettlementNotableController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TOW_Core.Utilities;
+
+namespace TOW_Core.CampaignSupport
+{
+    public class SettlementNotableController : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, new Action(this.CheckEmpireSettlements));
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+
+        private void CheckEmpireSettlements()
+        {
+            empireSettlements = Campaign.Current.Settlements.Where(s => (s.IsVillage || s.IsTown) && s.Culture.Name.Contains("Empire")).ToArray();
+
+            foreach (var settlement in empireSettlements)
+            {
+                foreach (var hero in settlement.Notables.ToArray())
+                {
+                    if (hero.IsNotable && hero.Age < 22)
+                    {
+                        ReplaceNotableVampire(hero, settlement);
+                        areThereKilledVampires = true;
+                    }
+                }
+            }
+            if (areThereKilledVampires)
+            {
+                TOWCommon.Say("The Inquisition carried out a cleanup");
+            }
+            areThereKilledVampires = false;
+        }
+
+        private void ReplaceNotableVampire(Hero vampire, Settlement settlement)
+        {
+            Occupation occupation = vampire.CharacterObject.Occupation;
+            KillCharacterAction.ApplyByRemove(vampire, false);
+            Hero newHero;
+
+            do
+            {
+                newHero = HeroCreator.CreateHeroAtOccupation(occupation, settlement);
+                if (newHero.Age < 22)
+                {
+                    KillCharacterAction.ApplyByRemove(newHero, false);
+                }
+            }
+            while (newHero.IsAlive);
+        }
+
+        private bool areThereKilledVampires;
+
+        private Settlement[] empireSettlements;
+    }
+}
--- CSharpSourceCode/SubModule.cs
+++ CSharpSourceCode/SubModule.cs
@@ -66,7 +66,7 @@ namespace TOW_Core
             harmony.PatchAll();
             ConfigureLogging();
 
-            
+
 
             //This has to be here.
             AbilityManager.LoadAbilities();
@@ -96,7 +96,7 @@ namespace TOW_Core
         }
 
         public void LoadFontAssets()
-		{
+        {
             UIResourceManager.SpriteData.SpriteCategories["tow_fonts"].Load(UIResourceManager.ResourceContext, UIResourceManager.UIResourceDepot);
         }
 
@@ -123,17 +123,17 @@ namespace TOW_Core
             UIResourceManager.SpriteData.SpriteCategories["ui_abilityicons"].Load(UIResourceManager.ResourceContext, UIResourceManager.UIResourceDepot);
             UIResourceManager.SpriteData.SpriteCategories["ui_hud"].Load(UIResourceManager.ResourceContext, UIResourceManager.UIResourceDepot);
             UIResourceManager.SpriteData.SpriteCategories["tow_gamemenu_backgrounds"].Load(UIResourceManager.ResourceContext, UIResourceManager.UIResourceDepot);
-		}
+        }
 
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             base.OnGameStart(game, gameStarterObject);
-            if(game.GameType is CustomGame)
+            if (game.GameType is CustomGame)
             {
                 gameStarterObject.Models.RemoveAllOfType(typeof(CustomBattleMoraleModel));
                 gameStarterObject.AddModel(new TOWBattleMoraleModel());
             }
-            else if(game.GameType is Campaign)
+            else if (game.GameType is Campaign)
             {
                 CampaignGameStarter starter = gameStarterObject as CampaignGameStarter;
 
@@ -147,6 +147,7 @@ namespace TOW_Core
                 starter.AddBehavior(new ChaosRaidingPartyCampaignBehavior());
                 starter.AddBehavior(new RaiseDeadInTownBehaviour());
                 starter.AddBehavior(new LibraryTownBehaviour());
+                starter.AddBehavior(new SettlementNotableController());
 
                 starter.Models.RemoveAllOfType(typeof(CompanionHiringPriceCalculationModel));
                 starter.Models.RemoveAllOfType(typeof(StoryModeEncounterGameMenuModel));
@@ -178,12 +179,12 @@ namespace TOW_Core
             mission.AddMissionBehaviour(new MagicWeaponEffectMissionLogic());
             mission.AddMissionBehaviour(new GrenadesMissionLogic());
             mission.AddMissionBehaviour(new AtmosphereOverrideMissionLogic());
-            if(Game.Current.GameType is Campaign)
+            if (Game.Current.GameType is Campaign)
             {
                 mission.AddMissionBehaviour(new BattleInfoMissionLogic());
             }
             //this is a hack, for some reason that is beyond my comprehension, this crashes the game when loading into an arena with a memory violation exception.
-            if(!mission.SceneName.Contains("arena")) mission.AddMissionBehaviour(new ShieldPatternsMissionLogic());
+            if (!mission.SceneName.Contains("arena")) mission.AddMissionBehaviour(new ShieldPatternsMissionLogic());
         }
 
         private void LoadStatusEffects()
