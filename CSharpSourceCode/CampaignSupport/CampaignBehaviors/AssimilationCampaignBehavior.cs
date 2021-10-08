da723c8c89c2abf1f44dbfb86e19820065615410
The Skull Throne <greendmitriy407@gmail.com>
2021-10-08 13:30:50 +0300

    added assimilation behavior and component

---
+++ CSharpSourceCode/CampaignSupport/CampaignBehaviors/AssimilationCampaignBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+
+namespace TOW_Core.CampaignSupport.CampaignBehaviors
+{
+    public class AssimilationCampaignBehavior : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.OnSettlementOwnerChangedEvent.AddNonSerializedListener(this, new Action<Settlement, bool, Hero, Hero, Hero, ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail>(OnOwnerChanged));
+            CampaignEvents.DailyTickSettlementEvent.AddNonSerializedListener(this, new Action<Settlement>(CheckForAssimilationComponent));
+        }
+
+        private void OnOwnerChanged(Settlement settlement, bool openToClaim, Hero newOwner, Hero oldOwner, Hero capturerHero, ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail detail)
+        {
+            if (settlement.IsTown && settlement.Culture != newOwner.MapFaction.Culture)
+            {
+                if (settlement.IsCastle)
+                {
+                    settlement.Culture = newOwner.MapFaction.Culture;
+                }
+
+                if (settlement.Town.Villages.Count > 0 || settlement.IsTown)
+                {
+                    var component = settlement.GetComponent<AssimilationComponent>();
+                    if (component == null)
+                    {
+                        settlement.AddComponent<AssimilationComponent>();
+                    }
+                    else
+                    {
+                        component.Reset();
+                    }
+                }
+            }
+        }
+
+        private void CheckForAssimilationComponent(Settlement settlement)
+        {
+            var component = settlement.GetComponent<AssimilationComponent>();
+            if (component != null)
+            {
+                if (!component.IsAssimilationComplete)
+                {
+                    component.Tick();
+                }
+            }
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+    }
+}
---
+++ CSharpSourceCode/CampaignSupport/SettlementComponents/AssimilationComponent.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace TOW_Core.CampaignSupport
+{
+    public class AssimilationComponent : SettlementComponent
+    {
+        public AssimilationComponent()
+        {
+        }
+
+        protected override void OnInventoryUpdated(ItemRosterElement item, int count)
+        {
+        }
+
+        public void Tick()
+        {
+            if (_assimilationProgress < 100)
+            {
+                if (_assimilationProgressRate == 0)
+                {
+                    if (Settlement.IsTown)
+                    {
+                        _assimilationProgressRate = 33 / Settlement.Town.Villages.Count + 1;
+                    }
+                    else
+                    {
+                        _assimilationProgressRate = 33 / Settlement.Town.Villages.Count;
+                    }
+                }
+                _assimilationProgress += _assimilationProgressRate;
+                if (_assimilationProgress > 50)
+                {
+                    Settlement.Culture = Settlement.MapFaction.Culture;
+                }
+                else if (_assimilationProgress > 66)
+                {
+                    var village = Settlement.Town.Villages.FirstOrDefault(v => v.Settlement.Culture != Settlement.Culture);
+                    if (village != null)
+                    {
+                        village.Settlement.Culture = Settlement.Culture;
+                    }
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            _assimilationProgress = 0;
+            _assimilationProgressRate = 0;
+        }
+
+
+        public bool IsAssimilationComplete { get => _assimilationProgress >= 100; }
+
+        private float _assimilationProgress;
+
+        private float _assimilationProgressRate;
+    }
+}
