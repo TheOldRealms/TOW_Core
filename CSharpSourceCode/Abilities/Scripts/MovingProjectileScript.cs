7c87b44095e0d8769cee90eb301db86a3cdfc042
Mart Kartasev <mart.kartasev@gmail.com>
2021-10-19 11:21:31 +0300

    Fix problem with spell hitting target

--- CSharpSourceCode/Abilities/Scripts/MovingProjectileScript.cs
+++ CSharpSourceCode/Abilities/Scripts/MovingProjectileScript.cs
@@ -7,12 +7,10 @@ namespace TOW_Core.Abilities.Scripts
         override protected bool CollidedWithAgent()
         {
             var collisionRadius = _ability.Template.Radius;
-            var closestAgent = Mission.Current.RayCastForClosestAgent(_previousFrameOrigin, GameEntity.GetGlobalFrame().origin, out float _, _casterAgent.Index, collisionRadius);
-            if (closestAgent != null)
-            {
-                return closestAgent.Index != _casterAgent.MountAgent.Index;
-            }
-            return false;
+            var index = _casterAgent.Health <= 0 ? -1 : _casterAgent.Index;
+            var closestAgent = Mission.Current.RayCastForClosestAgent(_previousFrameOrigin, GameEntity.GetGlobalFrame().origin, out float _, index, collisionRadius);
+            
+            return closestAgent != null && closestAgent.Index != _casterAgent.MountAgent?.Index;
         }
     }
 }
