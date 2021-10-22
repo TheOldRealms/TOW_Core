d444362b56567f384f7884286669972580410f72
Mart Kartasev <mart.kartasev@gmail.com>
2021-10-22 21:21:57 +0300

    Cleanup

--- CSharpSourceCode/Battle/TriggeredEffect/Scripts/SummonSkeleton.cs
+++ CSharpSourceCode/Battle/TriggeredEffect/Scripts/SummonSkeleton.cs
@@ -1,13 +1,4 @@
-﻿using System.Linq;
-using System.Collections.Generic;
-using System.Linq;
-using TaleWorlds.CampaignSystem;
-using TaleWorlds.Core;
-using TaleWorlds.Library;
-using TaleWorlds.MountAndBlade;
-using TaleWorlds.ObjectSystem;
-
-namespace TOW_Core.Battle.TriggeredEffect.Scripts
+﻿namespace TOW_Core.Battle.TriggeredEffect.Scripts
 {
     public class SummonSkeleton : SummonScript
     {
