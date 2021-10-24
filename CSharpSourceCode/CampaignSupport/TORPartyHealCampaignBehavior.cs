a1e5ed92812125432c48b08a5be83ff60fdb9d55
The Skull Throne <greendmitriy407@gmail.com>
2021-10-24 20:50:53 +0300

    added TORPartyHealCampaignBehavior
    set up additional amount of points to heal vampire heroes per hour

---
+++ CSharpSourceCode/CampaignSupport/TORPartyHealCampaignBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TOW_Core.Utilities.Extensions;
+
+namespace TOW_Core.CampaignSupport
+{
+    public class TORPartyHealCampaignBehavior : PartyHealCampaignBehavior
+    {
+        public override void RegisterEvents()
+        {
+            base.RegisterEvents();
+            CampaignEvents.HourlyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(HealParty));
+        }
+
+        private void HealParty(MobileParty party)
+        {
+            if (party.IsActive && party.MapEvent == null)
+            {
+                foreach (var troopRoster in party.MemberRoster.GetTroopRoster())
+                {
+                    if (troopRoster.Character.IsHero && troopRoster.Character.HeroObject.IsVampire())
+                    {
+                        troopRoster.Character.HeroObject.Heal(party.Party, 10, false);
+                    }
+                }
+            }
+        }
+    }
+}
