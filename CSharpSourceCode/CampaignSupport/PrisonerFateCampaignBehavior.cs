ee50cf822db34e2af5cdd99e3e931d0728718af8
The Skull Throne <greendmitriy407@gmail.com>
2021-10-25 17:56:59 +0300

    added PrisonerFateCamapignBehavior

---
+++ CSharpSourceCode/CampaignSupport/PrisonerFateCampaignBehavior.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.ObjectSystem;
+using TOW_Core.Utilities;
+
+namespace TOW_Core.CampaignSupport
+{
+    public class PrisonerFateCampaignBehavior : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, Initialize);
+            CampaignEvents.MapEventEnded.AddNonSerializedListener(this, OnMapEventEnded);
+        }
+
+        private void Initialize(CampaignGameStarter obj)
+        {
+            _skeleton = MBObjectManager.Instance.GetObject<CharacterObject>("tow_skeleton_recruit");
+        }
+
+        private void OnMapEventEnded(MapEvent mapEvent)
+        {
+            if (mapEvent.HasWinner)
+            {
+                foreach (var party in mapEvent.Winner.Parties)
+                {
+                    if (party.IsNpcParty && (!party.Party.IsMobile || party.Party.MobileParty.IsLordParty))
+                    {
+                        switch (party.Party.Culture.StringId)
+                        {
+                            case "empire":
+                            case "chaos":
+                                KillRegularPrisoners(party);
+                                //FilterNewMemebers(party, true);
+                                break;
+                            case "khuzait":
+                                SacrificePrisoners(party);
+                                //FilterNewMemebers(party, false);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FilterNewMemebers(MapEventParty party, bool shouldKill)
+        {
+            foreach (var troop in party.RosterToReceiveLootMembers.GetTroopRoster())
+            {
+                if (troop.Character.Culture != party.Party.Culture)
+                {
+                    if (!shouldKill)
+                    {
+                    //    party.RosterToReceiveLootPrisoners.Add()
+                    }
+                    party.RosterToReceiveLootMembers.RemoveTroop(troop.Character, troop.Number);
+                }
+            }
+        }
+
+        private void KillRegularPrisoners(MapEventParty party)
+        {
+            TOWCommon.Say($"{party.Party} killed {party.RosterToReceiveLootPrisoners.TotalRegulars} prisoners");
+            foreach (var troop in party.RosterToReceiveLootPrisoners.GetTroopRoster())
+            {
+                if (!troop.Character.IsHero)
+                {
+                    party.RosterToReceiveLootPrisoners.RemoveTroop(troop.Character, troop.Number);
+                }
+            }
+        }
+
+        private void SacrificePrisoners(MapEventParty party)
+        {
+            var number = Math.Min(party.RosterToReceiveLootPrisoners.TotalRegulars, party.Party.PartySizeLimit - party.Party.MemberRoster.TotalManCount);
+            KillRegularPrisoners(party);
+            TOWCommon.Say($"{party.Party} raised {number} skeletons");
+            if (_skeleton != null)
+            {
+                party.Party.MemberRoster.AddToCounts(_skeleton, number);
+            }
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+
+        private CharacterObject _skeleton;
+    }
+}
