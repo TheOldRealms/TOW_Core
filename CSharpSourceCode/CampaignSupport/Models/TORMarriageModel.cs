2d62ed5d35ffc2ddfa090d295f9075cb63e061b9
The Skull Throne <greendmitriy407@gmail.com>
2021-10-28 16:51:37 +0300

    created TORMarriageModel

---
+++ CSharpSourceCode/CampaignSupport/Models/TORMarriageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace TOW_Core.CampaignSupport.Models
+{
+    public class TORMarriageModel : DefaultMarriageModel
+    {
+        public override bool IsCoupleSuitableForMarriage(Hero firstHero, Hero secondHero)
+        {
+            return false;
+        }
+
+        public override bool IsSuitableForMarriage(Hero maidenOrSuitor)
+        {
+            return false;
+        }
+    }
+}
