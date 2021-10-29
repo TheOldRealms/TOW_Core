88b2defc928bcf76bc8348bcaabe2cde88c80d91
Dmitriy <62873054+Badulundabad@users.noreply.github.com>
2021-10-29 19:22:28 +0300

    Marriage disabling (#206)
    * created TORMarriageModel
    
    * made some little changes

---
+++ CSharpSourceCode/CampaignSupport/Models/TORMarriageModel.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace TOW_Core.CampaignSupport.Models
+{
+    public class TORMarriageModel : DefaultMarriageModel
+    {
+        public override bool IsCoupleSuitableForMarriage(Hero firstHero, Hero secondHero)
+        {
+            return false;
+        }
+
+        public override bool IsSuitableForMarriage(Hero maidenOrSuitor)
+        {
+            return false;
+        }
+    }
+}
