48d8704a73fc4061112210deb7abecb60a029f2f
The Skull Throne <greendmitriy407@gmail.com>
2021-11-07 20:57:50 +0300

    Add some changes
    - created TORAgentStatCalculateModel to increase vampire speed;
    - added Mission.SpawnAgent harmony patch to replace usual monster by custom one for vampires. New monster get vampires able to jump higher;
    - added Mission.FallDamageCallback harmony patch to avoid fall damage for vampires;

--- CSharpSourceCode/HarmonyPatches/MissionPatches.cs
+++ CSharpSourceCode/HarmonyPatches/MissionPatches.cs
@@ -1,5 +1,11 @@
 ﻿using HarmonyLib;
+using System.Xml;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.ModuleManager;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
+using TOW_Core.Utilities.Extensions;
 
 [HarmonyPatch]
 public static class MissionPatches
@@ -11,5 +17,41 @@ public static class MissionPatches
     public static void ComputeBlowMagnitudeMissilePostfix(ref float baseMagnitude)
     {
         baseMagnitude = baseMagnitude <= limit ? baseMagnitude : limit;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Mission), "SpawnAgent")]
+    public static bool SpawnAgentPrefixprivate(AgentBuildData agentBuildData)
+    {
+        var character = agentBuildData.AgentCharacter;
+        if (character != null && character.IsHero && (character as CharacterObject).IsVampire())
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(ModuleHelper.GetModuleFullPath("TOW_Core") + "ModuleData/tor_monsters.xml");
+            foreach (var node in xmlDoc.ChildNodes.Item(1).ChildNodes)
+            {
+                XmlNode xmlNode = (XmlNode)node;
+                var monster = new Monster();
+                monster.Deserialize(MBObjectManager.Instance, xmlNode);
+                Traverse.Create(agentBuildData).Property("AgentData").Field("_agentMonster").SetValue(monster);
+                break;
+            }
+        }
+        return true;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Mission), "FallDamageCallback")]
+    public static bool FallDamageCallbackPrefix(ref AttackCollisionData collisionData, Blow b, Agent attacker, Agent victim)
+    {
+        if (victim.IsHuman && victim.Character != null)
+        {
+            var character = victim.Character as CharacterObject;
+            if (character.IsHero && character.HeroObject.IsVampire())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
