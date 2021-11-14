91338a4684bd4e763b7fe1d80f122e70ba948881
The Skull Throne <greendmitriy407@gmail.com>
2021-11-14 19:42:27 +0300

    reworked shadow step logic

--- CSharpSourceCode/Abilities/Scripts/ShadowStepScript.cs
+++ CSharpSourceCode/Abilities/Scripts/ShadowStepScript.cs
@@ -2,73 +2,98 @@
 using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TOW_Core.Utilities;
 using TOW_Core.Utilities.Extensions;
 
 namespace TOW_Core.Abilities.Scripts
 {
     public class ShadowStepScript : AbilityScript
     {
-        protected override void OnTick(float dt)
+        public void Initialize(Agent agent)
         {
-            if (_isFading)
-            {
-                return;
-            }
-            if (!_hasTriggered)
-            {
-                _hasTriggered = true;
-                _abilityLife = 0;
-                _casterAgent.SetInvulnerable(true);
-                _casterAgent.Disappear();
-                _speed = _ability.Template.BaseMovementSpeed / 2;
+            _casterAgent = agent;
+            _speed = _ability.Template.BaseMovementSpeed / 2;
+            DisbindKeyBindings();
+            var frame = _casterAgent.Frame.Elevate(1f);
+            GameEntity.SetGlobalFrame(frame);
+            GameEntity.BodyFlag = BodyFlags.Barrier3D;
+            GameEntity.BodyFlag |= BodyFlags.DontCollideWithCamera;
+            //Scale(0.7f);
+            //void Scale(float scaling)
+            //{
+            //    var localFrame = GameEntity.GetFrame();
+            //    localFrame.Scale(new Vec3(scaling, scaling, scaling));
+            //    GameEntity.SetFrame(ref localFrame);
+            //}
+        }
 
-                var frame = _casterAgent.Frame.Elevate(1);
-                var sphere = GameEntity.Instantiate(Scene, "magic_sphere", MatrixFrame.Identity);
-                sphere.AddBodyFlags(BodyFlags.DontCollideWithCamera);
-                sphere.AddBodyFlags(BodyFlags.Barrier3D);
-                sphere.EntityVisibilityFlags = EntityVisibilityFlags.VisibleOnlyForEnvmap;
-                GameEntity.SetGlobalFrame(frame);
-                GameEntity.AddChild(sphere);
-            }
-            else
+        private void BindKeyBindings()
+        {
+            _keyContext.GetGameKey(0).KeyboardKey.ChangeKey(_axisKeys[0]);
+            _keyContext.GetGameKey(1).KeyboardKey.ChangeKey(_axisKeys[1]);
+            _keyContext.GetGameKey(2).KeyboardKey.ChangeKey(_axisKeys[2]);
+            _keyContext.GetGameKey(3).KeyboardKey.ChangeKey(_axisKeys[3]);
+        }
+
+        private void DisbindKeyBindings()
+        {
+            for (int i = 0; i < 4; i++)
             {
-                _abilityLife += dt;
-                ChangePosition();
-                if (_abilityLife > _ability.Template.Duration && !_isFading)
-                {
-                    Stop();
-                }
+                _axisKeys[i] = _keyContext.GetGameKey(i).KeyboardKey.InputKey;
+                _keyContext.GetGameKey(i).KeyboardKey.ChangeKey(InputKey.Invalid);
             }
         }
 
-        private void ChangePosition()
+        protected override void OnTick(float dt)
         {
-            MatrixFrame frame = GameEntity.GetFrame();
-            frame.rotation = Agent.Main.LookRotation;
-            if (Input.IsKeyPressed(InputKey.W) || Input.IsKeyDown(InputKey.W))
+            if (_casterAgent != null && _casterAgent.Health > 0)
             {
+                var dist = GetDistance();
+                if (Input.IsKeyDown(InputKey.W) || Input.IsKeyPressed(InputKey.W))
+                {
+                    if (dist > 3)
+                    {
+                        Fly();
+                        TOWCommon.Say($"DIST {dist}");
+                    }
+                    else if (dist.Equals(float.NaN))
+                    {
+                        Fly();
+                        TOWCommon.Say($"NaN {dist}");
+                    }
+                    else
+                    {
+                        TOWCommon.Say($"KEY {dist}");
+                    }
+                }
+                if (Input.IsKeyPressed(InputKey.RightMouseButton))
+                {
+                    Stop();
+                }
+
+            }
+            float GetDistance()
+            {
+                float num;
+                Vec3 pos = _casterAgent.LookFrame.Advance(3).origin;
+                Mission.Current.Scene.RayCastForClosestEntityOrTerrain(_casterAgent.Position + new Vec3(0f, 0f, _casterAgent.GetEyeGlobalHeight(), -1f), pos, out num);
+                return num;
+            }
+            void Fly()
+            {
+                MatrixFrame frame = GameEntity.GetGlobalFrame();
+                frame.rotation = _casterAgent.LookRotation;
                 frame.Advance(_speed);
+                GameEntity.SetGlobalFrame(frame);
             }
-            if (Input.IsKeyPressed(InputKey.S) || Input.IsKeyDown(InputKey.S))
-            {
-                frame.Advance(-_speed);
-            }
-            if (Input.IsKeyPressed(InputKey.A) || Input.IsKeyDown(InputKey.A))
-            {
-                frame.Strafe(-_speed);
-            }
-            if (Input.IsKeyPressed(InputKey.D) || Input.IsKeyDown(InputKey.D))
-            {
-                frame.Strafe(_speed);
-            }
-            GameEntity.SetGlobalFrame(frame);
         }
 
         public void Stop()
         {
             GameEntity.FadeOut(0.05f, true);
-            _casterAgent.Appear();
-            _casterAgent.SetInvulnerable(false);
+            BindKeyBindings();
+            base._casterAgent.Appear();
+            base._casterAgent.SetInvulnerable(false);
             _isFading = true;
         }
 
@@ -76,5 +101,7 @@ namespace TOW_Core.Abilities.Scripts
         public bool IsFadinOut { get => _isFading; }
 
         private float _speed;
+        private InputKey[] _axisKeys = new InputKey[4];
+        private GameKeyContext _keyContext = HotKeyManager.GetCategory("CombatHotKeyCategory");
     }
 }
