29c71525d38acd4604405b35187de56d295b33da
hunharibo <hunharibo@gmail.com>
2021-12-06 21:19:31 +0100

    Create ItemObjectPatches.cs

---
+++ CSharpSourceCode/HarmonyPatches/ItemObjectPatches.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using TaleWorlds.Core;
+
+namespace TOW_Core.HarmonyPatches
+{
+    [HarmonyPatch]
+    public class ItemObjectPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ItemObject), "GetAirFrictionConstant")]
+        public static void PostFix(ref float __result, WeaponClass ___weaponClass)
+        {
+            if(___weaponClass == WeaponClass.Boulder)
+            {
+                __result = 0.0001f;
+            }
+        }
+    }
+}
