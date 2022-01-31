e896611ecef650db2d311d86ff7999cb7ff02aca
Linus Tiemann <linus-tiemann@live.de>
2022-01-31 01:22:43 +0100

    DotIds seperated

--- CSharpSourceCode/ObjectDataExtensions/SpellBlowInfoManager.cs
+++ CSharpSourceCode/ObjectDataExtensions/SpellBlowInfoManager.cs
@@ -7,6 +7,50 @@ namespace TOW_Core.ObjectDataExtensions
     public static class SpellBlowInfoManager
     {
         private static Dictionary<int, Queue<SpellInfo>> SpellIDs;
+        private static Dictionary<int, Queue<SpellInfo>> DotIDs;
+        
+        
+        public static void EnqueueDotInfo(int agentIndex, string dotName, DamageType damageType)
+        {
+            if (agentIndex == -1)
+                return;
+            
+            if (DotIDs == null)
+            {
+                DotIDs = new Dictionary<int, Queue<SpellInfo>>();
+            }
+            
+            if(DotIDs.ContainsKey(agentIndex))
+            {
+                SpellInfo info = new SpellInfo();
+                info.SpellID = dotName;
+                info.DamageType = damageType;
+                DotIDs[agentIndex].Enqueue(info);
+                return;
+            }
+            
+            var spellItem = new SpellInfo();
+            spellItem.SpellID = dotName;
+            spellItem.DamageType = damageType;
+            Queue<SpellInfo> queue = new Queue<SpellInfo>();
+            queue.Enqueue(spellItem);
+            DotIDs.Add(agentIndex, queue);
+        }
+        
+        public static  SpellInfo GetDotInfo(int agentIndex)
+        {
+            if (!DotIDs.ContainsKey(agentIndex)) return new SpellInfo();
+            var item = DotIDs[agentIndex].Dequeue();
+
+            if (!DotIDs[agentIndex].IsEmpty())
+            {
+                return item;
+            }
+
+            DotIDs.Remove(agentIndex);
+            return item;
+        }
+        
         
         public static void EnqueueSpellInfo(int agentIndex, string spellName, DamageType damageType)
         {
