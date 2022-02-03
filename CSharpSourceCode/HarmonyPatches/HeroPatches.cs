31f2b4299cdc6592d0896f474d64876b94e3f8ee
The Skull Throne <greendmitriy407@gmail.com>
2022-02-03 15:17:27 +0300

    fixed hero patch

--- CSharpSourceCode/HarmonyPatches/HeroPatches.cs
+++ CSharpSourceCode/HarmonyPatches/HeroPatches.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
+﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
-using TOW_Core.Utilities.Extensions;
 
 namespace TOW_Core.HarmonyPatches
 {
@@ -17,16 +11,17 @@ namespace TOW_Core.HarmonyPatches
         [HarmonyPatch(typeof(Hero), "BodyProperties", MethodType.Getter)]
         public static void PostFix(ref BodyProperties __result, Hero __instance)
         {
-            if(__instance.HasAttribute("VampireBodyOverride"))
+            if (__instance.IsNotable)
             {
-                try
+                if (__instance.Culture.Name.Contains("Vampire"))
                 {
                     StaticBodyProperties staticBodyProperties = (StaticBodyProperties)Traverse.Create(__instance).Property("StaticBodyProperties").GetValue();
                     __result = new BodyProperties(new DynamicBodyProperties(19, __instance.Weight, __instance.Build), staticBodyProperties);
                 }
-                catch(Exception)
+                else if (!__instance.IsFemale)
                 {
-                    TOW_Core.Utilities.TOWCommon.Log("Attempted to override BodyProperties for Hero, but failed.", NLog.LogLevel.Error);
+                    StaticBodyProperties staticBodyProperties = (StaticBodyProperties)Traverse.Create(__instance).Property("StaticBodyProperties").GetValue();
+                    __result = new BodyProperties(new DynamicBodyProperties(21, __instance.Weight, __instance.Build), staticBodyProperties);
                 }
             }
         }
