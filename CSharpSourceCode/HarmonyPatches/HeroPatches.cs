df52c10bf1de8be2ea21701ee529bf4f458e5fdf
The Skull Throne <greendmitriy407@gmail.com>
2022-02-05 21:14:04 +0300

    fixed HeroPatch

--- CSharpSourceCode/HarmonyPatches/HeroPatches.cs
+++ CSharpSourceCode/HarmonyPatches/HeroPatches.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
+using TOW_Core.Utilities.Extensions;
 
 namespace TOW_Core.HarmonyPatches
 {
@@ -11,18 +12,15 @@ namespace TOW_Core.HarmonyPatches
         [HarmonyPatch(typeof(Hero), "BodyProperties", MethodType.Getter)]
         public static void PostFix(ref BodyProperties __result, Hero __instance)
         {
-            if (__instance.IsNotable)
+            if (__instance.IsVampire())
             {
-                if (__instance.Culture.Name.Contains("Vampire"))
-                {
-                    StaticBodyProperties staticBodyProperties = (StaticBodyProperties)Traverse.Create(__instance).Property("StaticBodyProperties").GetValue();
-                    __result = new BodyProperties(new DynamicBodyProperties(19, __instance.Weight, __instance.Build), staticBodyProperties);
-                }
-                else if (!__instance.IsFemale)
-                {
-                    StaticBodyProperties staticBodyProperties = (StaticBodyProperties)Traverse.Create(__instance).Property("StaticBodyProperties").GetValue();
-                    __result = new BodyProperties(new DynamicBodyProperties(21, __instance.Weight, __instance.Build), staticBodyProperties);
-                }
+                StaticBodyProperties staticBodyProperties = (StaticBodyProperties)Traverse.Create(__instance).Property("StaticBodyProperties").GetValue();
+                __result = new BodyProperties(new DynamicBodyProperties(19, __instance.Weight, __instance.Build), staticBodyProperties);
+            }
+            else if (!__instance.IsFemale)
+            {
+                StaticBodyProperties staticBodyProperties = (StaticBodyProperties)Traverse.Create(__instance).Property("StaticBodyProperties").GetValue();
+                __result = new BodyProperties(new DynamicBodyProperties(21, __instance.Weight, __instance.Build), staticBodyProperties);
             }
         }
     }
