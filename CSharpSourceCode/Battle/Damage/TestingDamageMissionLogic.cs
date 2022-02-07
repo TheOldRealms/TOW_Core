bca232bf2453c600561ac659685ce394501901c6
The Skull Throne <greendmitriy407@gmail.com>
2022-02-07 21:19:00 +0300

    adjusted CannonBallScript. added test method

--- CSharpSourceCode/Battle/Damage/TestingDamageMissionLogic.cs
+++ CSharpSourceCode/Battle/Damage/TestingDamageMissionLogic.cs
@@ -6,25 +6,43 @@ namespace TOW_Core.Battle.Damage
     /// <summary>Class <c>TestingDamageMissionLogic</c> A mission logic that can be used for debugging and testing.
     /// It should contain methods and mission logics  creating and deleting Agent entities in a mission on the fly and testing damage related features.
     ///</summary>
-    public class TestingDamageMissionLogic: MissionLogic
+    public class TestingDamageMissionLogic : MissionLogic
     {
+        private static float _slowMotionEndTime;
+
         public override void OnMissionTick(float dt)
         {
             base.OnMissionTick(dt);
-            
+            CheckForSlowMotionTime();
+            //SomeZ3rcasMethod();
+        }
+
+        public static void EnableSlowMotion(float time)
+        {
+            _slowMotionEndTime = Mission.Current.CurrentTime + time;
+            Mission.Current.Scene.SlowMotionMode = true;
+        }
+
+        private void CheckForSlowMotionTime()
+        {
+            Mission.Scene.SlowMotionMode = _slowMotionEndTime < Mission.CurrentTime ? false : true;
+        }
+
+        private void SomeZ3rcasMethod()
+        {
             if (Input.IsKeyPressed(InputKey.N))
             {
                 Blow b = new Blow();
-                
-                
+
+
                 if (Mission.Current.PlayerTeam.IsAttacker)
                 {
-                    if(Mission.Teams.Defender.Leader.Health>0)
+                    if (Mission.Teams.Defender.Leader.Health > 0)
                         Mission.Teams.Defender.Leader.Die(b);
                 }
                 else
                 {
-                    if(Mission.Teams.Defender.Leader.Health>0)
+                    if (Mission.Teams.Defender.Leader.Health > 0)
                         Mission.Teams.Attacker.Leader.Die(b);
                 }
             }
--- CSharpSourceCode/Battle/TriggeredEffect/Scripts/CannonBallScript.cs
+++ CSharpSourceCode/Battle/TriggeredEffect/Scripts/CannonBallScript.cs
@@ -3,6 +3,7 @@ using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TOW_Core.Battle.Damage;
 using TOW_Core.Utilities.Extensions;
 
 namespace TOW_Core.Battle.TriggeredEffect.Scripts
@@ -32,48 +33,83 @@ namespace TOW_Core.Battle.TriggeredEffect.Scripts
 
         protected override void OnRemoved(int removeReason)
         {
-            var distanceFromPlayer = GameEntity.GlobalPosition.Distance(Mission.Current.GetCameraFrame().origin);
-            RunVisualAndSoundEffects(distanceFromPlayer);
+            //RunVisualEffects();
+            RunSoundEffects();
 
-            var agentsToTearApart = Mission.Current.GetNearbyAgents(GameEntity.GlobalPosition.AsVec2, _explosionRadius).ToArray();
-            for (int i = 0; i < agentsToTearApart.Length; i++)
+            var nearbyAgents = Mission.Current.GetNearbyAgents(GameEntity.GlobalPosition.AsVec2, _explosionRadius).ToArray();
+            for (int i = 0; i < nearbyAgents.Length; i++)
             {
-                var agent = agentsToTearApart[i];
-                if (distanceFromPlayer < _explosionRadius)
+                var agent = nearbyAgents[i];
+                var distance = agent.Position.Distance(GameEntity.GlobalPosition);
+                if (distance < _explosionRadius)
                 {
-                    var damage = (_explosionRadius - distanceFromPlayer) / _explosionRadius * _damage;
+                    var damage = (_explosionRadius - distance) / _explosionRadius * _damage;
                     agent.ApplyDamage((int)damage, _shooterAgent, doBlow: true, hasShockWave: true, impactPosition: GameEntity.GlobalPosition);
-                    if (distanceFromPlayer < 2 && agent.State == AgentState.Killed)
+                    if (distance < 3 && agent.State == AgentState.Killed)
                     {
+                        TestingDamageMissionLogic.EnableSlowMotion(3);
                         agent.Disappear();
                         var frame = agent.Frame.Elevate(1);
-                        LaunchLimb(frame, "exploded_torso_001");
-                        LaunchLimb(frame, "exploded_torso_001");
-                        LaunchLimb(frame, "exploded_torso_001");
-                        LaunchLimb(frame, "exploded_torso_001");
-                        LaunchLimb(frame, "exploded_torso_001");
+                        if (distance <= 1.5f)
+                        {
+                            ExplodeNearVictim(frame);
+                        }
+                        else
+                        {
+                            ExplodeFarVictim(frame);
+                        }
                     }
                 }
             }
         }
 
-        private void RunVisualAndSoundEffects(float distanceFromPlayer)
+        private void RunVisualEffects()
         {
-            //Visual effect
             var effect = GameEntity.CreateEmpty(Mission.Current.Scene);
             MatrixFrame frame = MatrixFrame.Identity;
             ParticleSystem.CreateParticleSystemAttachedToEntity("cannonball_explosion_6", effect, ref frame);
             var globalFrame = new MatrixFrame(Mat3.CreateMat3WithForward(in Vec3.Zero), GameEntity.GlobalPosition);
             effect.SetGlobalFrame(globalFrame);
             effect.FadeOut(10, true);
+        }
 
-            //Sound effect
+        private void RunSoundEffects()
+        {
+            var distanceFromPlayer = GameEntity.GlobalPosition.Distance(Mission.Current.GetCameraFrame().origin);
             int soundIndex = distanceFromPlayer < 30 ? SoundEvent.GetEventIdFromString("cannonball_explosion_close") : SoundEvent.GetEventIdFromString("cannonball_explosion_far");
             _sound = SoundEvent.CreateEvent(soundIndex, Mission.Current.Scene);
             if (_sound != null)
             {
                 _sound.PlayInPosition(GameEntity.GlobalPosition);
             }
+        }
+
+        private void ExplodeNearVictim(MatrixFrame frame)
+        {
+            LaunchLimb(frame, "exploded_head_001"); 
+            LaunchLimb(frame, "exploded_arms_001");
+            LaunchLimb(frame, "exploded_arms_002");
+            LaunchLimb(frame, "exploded_legs_002");
+            LaunchLimb(frame, "exploded_legs_003");
+
+            LaunchLimb(frame, "exploded_flesh_pieces_001");
+            LaunchLimb(frame, "exploded_flesh_pieces_002");
+            LaunchLimb(frame, "exploded_flesh_pieces_003");
+
+            LaunchLimb(frame, "exploded_limb_pieces_001");
+            LaunchLimb(frame, "exploded_limb_pieces_002");
+            LaunchLimb(frame, "exploded_limb_pieces_003");
+        }
+
+        private void ExplodeFarVictim(MatrixFrame frame)
+        {
+            LaunchLimb(frame, "exploded_torso_001");
+            LaunchLimb(frame, "exploded_legs_001");
+
+            LaunchLimb(frame, "exploded_flesh_pieces_001");
+            LaunchLimb(frame, "exploded_flesh_pieces_002");
+            LaunchLimb(frame, "exploded_limb_pieces_001");
+            LaunchLimb(frame, "exploded_limb_pieces_002");
         }
 
         private void LaunchLimb(MatrixFrame frame, string name)
@@ -84,7 +120,8 @@ namespace TOW_Core.Battle.TriggeredEffect.Scripts
             limb.CallScriptCallbacks();
             var dir = GetRandomDirection();
             limb.EnableDynamicBody();
-            limb.AddPhysics(limb.Mass, limb.CenterOfMass, limb.GetBodyShape(), dir * 5, dir * 2, PhysicsMaterial.GetFromName("flesh"), false, -1);
+            var multiplier = 30 / limb.Mass;
+            limb.AddPhysics(limb.Mass, limb.CenterOfMass, limb.GetBodyShape(), dir * multiplier, dir * 2, PhysicsMaterial.GetFromName("flesh"), false, -1);
         }
 
         private Vec3 GetRandomDirection()
