5709671ae89f27e74e17a0a5e4b8f2d171295da7
Mart Kartašev <mart.kartasev@gmail.com>
2022-04-07 17:17:34 +0200

    Fix targeting a little bit

--- CSharpSourceCode/Battle/AI/AgentBehavior/AgentCastingBehavior/MissileCastingBehavior.cs
+++ CSharpSourceCode/Battle/AI/AgentBehavior/AgentCastingBehavior/MissileCastingBehavior.cs
@@ -22,24 +22,23 @@ namespace TOW_Core.Battle.AI.AgentBehavior.AgentCastingBehavior
             var targetFormation = CurrentTarget.Formation;
             var medianAgent = targetFormation?.GetMedianAgent(true, false, targetFormation.GetAveragePositionOfUnits(true, false));
             if (medianAgent == null) return target;
-            
+
             if (targetFormation.CountOfUnits > 10)
             {
                 var adjustedPosition = medianAgent.Position;
-                adjustedPosition += ComputeSpellAngleVelocityCorrection(medianAgent.Position, medianAgent.Velocity);
-
-
-                {
-                    var direction = targetFormation.QuerySystem.EstimatedDirection;
-                    var rightVec = direction.RightVec();
-                    adjustedPosition += direction.ToVec3() * (float) (_random.NextDouble() * targetFormation.Depth - targetFormation.Depth / 2);
-                    adjustedPosition += rightVec.ToVec3() * (float) (_random.NextDouble() * targetFormation.Width - 2 - (targetFormation.Width - 1) / 2);
-                }
-
-                adjustedPosition.z = Mission.Current.Scene.GetGroundHeightAtPosition(adjustedPosition) + 0.25f;
-                target.SelectedWorldPosition = adjustedPosition;
-
+                
+                var direction = targetFormation.QuerySystem.EstimatedDirection;
+                var rightVec = direction.RightVec();
+                
+                adjustedPosition += direction.ToVec3() * (float) (_random.NextDouble() * targetFormation.Depth - targetFormation.Depth / 2);
+                adjustedPosition += rightVec.ToVec3() * (float) (_random.NextDouble() * targetFormation.Width - 2 - (targetFormation.Width - 1) / 2);
+                
                 medianAgent = targetFormation.GetMedianAgent(true, false, adjustedPosition.AsVec2);
+                target.Agent = medianAgent;
+                
+                adjustedPosition = medianAgent.Position;
+                adjustedPosition += ComputeSpellAngleVelocityCorrection(medianAgent.Position, medianAgent.Velocity);
+                target.SelectedWorldPosition = adjustedPosition;
             }
 
             target.Agent = medianAgent;
