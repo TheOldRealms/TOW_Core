0b21b4db8f40912e778a9ab2c0b228bb50bab73d
jasonleh99 <jasonleh99@gmail.com>
2022-05-04 00:22:30 -0700

    Edited the default learning time to avoid situations where we may divide by 0

--- CSharpSourceCode/Items/ItemTrait.cs
+++ CSharpSourceCode/Items/ItemTrait.cs
@@ -52,6 +52,6 @@ namespace TOW_Core.Items
         [XmlAttribute]
         public int FlatSkillModifier { get; set; } = 0;
         [XmlAttribute]
-        public float LearningTime { get; set; } = 0;
+        public float LearningTime { get; set; } = 1;
     }
 }
